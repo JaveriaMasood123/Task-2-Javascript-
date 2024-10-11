61c9dcffddb8e0ccbec0934d0235abb11e9e84f9
Javeria Masood <javeriamails@gmail.com>
2024-10-11 12:21:47 +0500

    Add files via upload

---
+++ task 2 javascript/app.js
@@ -0,0 +1,89 @@
+// 1. Write a JavaScript program that asks the user for their age and then displays a message indicating whether they are eligible to vote (18 or older).
+
+// let age=prompt("enter a age");
+// if(age>=18){
+//     console.log("Eligible for vote");
+//     }
+// else{
+//     console.log("Not eligible for vote");
+// }
+
+//2. Use a conditional statement to determine whether a number is positive, negative, or zero.
+
+// let value=prompt("enter a number");
+// if(value>0){
+//     console.log("Number is positive");
+//     }
+// else if(value<0){
+//     console.log("Number is negative");
+//     }
+// else{
+//     console.log("Number is zero");
+// }
+
+//3. Write a JavaScript program that asks the user for their favorite color and then displays a message indicating whether they like a primary color (red, blue, or yellow, green).
+
+// let color=prompt(" enter your favorite color ");
+// if(color=="red"||color=="blue"|| color=="yellow"||color=="green"){
+//     console.log("you like a primary color");
+// }
+// else{
+//     console.log("you don't like a primary color");
+// }
+
+//4. Use logical operators to determine whether a number is between 10 and 20 (use logical operator).
+ 
+// let num=prompt("enter a number");
+// if(num>=10 && num<=20){
+//     console.log("number is between 10 and 20 ");
+//     }
+// else{
+//     console.log("number is not between 10 and 20 "); 
+// }
+
+// 5. Write a JavaScript program that asks the user for their grade (A, B, C, D, or F) and then displays a message indicating whether they passed or failed.
+
+// let grade=prompt("enter your grade");
+// if(grade=="A"||grade=="B"||grade=="C"||grade=="C"||grade=="E"||  grade=="F" ){
+//     console.log("passed"); 
+// }
+// else{
+//     console.log("failed"); 
+// }
+
+//6. Write a JavaScript program that asks the user for their age and gender, and then displays a message indicating whether they are eligible to serve in the military (18-30 years old, male).
+
+// let age=prompt("enter your age");
+// let gender =prompt("enter your gender");
+// if(age>=18 && age<=30 && gender =="male"){
+//     console.log("you are eligible to serve in the military")
+// }
+// else {
+//     console.log("you are not eligible to serve in the military");
+// }
+
+// 7. Use logical operators to determine whether a number is even or odd.
+ 
+// let number=prompt("enter a number");
+// if(number%2==0){
+//     console.log("number is even");
+//     }
+// else{
+//     console.log("number is odd");
+    
+// }
+
+// 8. Use a conditional statement to determine whether a number is a multiple of 3.
+
+let number=prompt("enter a number");
+if(number%3==0){
+    console.log("multiple of 3");
+    }
+else{
+    console.log("not multiple of 3");
+    
+}
+
+
+
+
