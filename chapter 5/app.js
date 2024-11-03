eab414ff1e11826527303c1c08737feb11d7231e
Javeria Masood <javeriamails@gmail.com>
2024-11-03 20:33:15 +0500

    Add files via upload

---
+++ chapter 5/app.js
@@ -0,0 +1,93 @@
+//     MATHS EXPRESSIONS
+//      ASSIGNMENT # 6
+
+  
+//ANSWER #1
+// let a=10;
+// document.write("Result: <br> The value of a is: ", a);
+// document.write("<br>...................................<br>");
+// document.write("<br>The value of ++a is: ", ++a);
+// document.write("<br>Now the value of a is: ", a);
+// document.write("<br><br>The value of ++a is: ", ++a);
+// document.write("<br>Now the value of a is: ", a);
+// document.write("<br><br>The value of --a is: ", --a);
+// document.write("<br>Now the value of a is: ", a);
+// document.write("<br><br>The value of a-- is: ", a--);
+// document.write("<br>Now the value of a is: ", a);
+
+
+//ANSWER #2
+// var a=2;
+// var b=1;
+// document.write("value of a is =  ", a, " <br> value of b is = ",b);
+
+// --a;
+// document.write("<br> After --a: ", a);
+
+// a=2;
+// b=1;
+
+// --a - --b;
+// document.write("<br> After --a - --b: ",--a - --b);
+
+// a=2;
+// b=1;
+// --a - --b + ++b;
+// document.write("<br> After --a - --b + ++b: ",--a - --b + ++b);
+
+// a=2;
+// b=1;
+// var result= --a - --b + ++b + b--;
+// document.write("<br> After --a - --b + ++b + b--: ",result);
+
+// document.write("<br>a =",a);
+// document.write("<br>b =",b);
+// document.write("<br>result =",result);
+
+
+//ANSWER #3
+// let user_name=prompt("enter your name");
+// alert("hello "+ user_name);
+
+//ANSWER #4
+// let num=prompt("enter the number");
+// if(num==""){
+//    for(i=1;i<=10;i++){
+//     document.write("5" ,"x", i , "=" ,5 *i, "<br>")
+    
+// }
+// }
+// else{
+// for(i=1;i<=10;i++){
+   
+//     document.write(num ,"x", i , "=" ,num*i, "<br>")
+// }
+// }
+
+
+//       ANSWER #5
+ //let subject1=prompt("enter the first subject name");
+//  let subject2=prompt("enter the second subject name");
+//  let subject3=prompt("enter the third subject name");
+
+//  let total_marks=100;
+//  let obtained_marks1=parseInt(prompt("enter the marks of first subject "));
+//  let obtained_marks2=parseInt(prompt("enter the marks of second subject "));
+//  let obtained_marks3=parseInt(prompt("enter the marks of third subject"));
+ 
+// let total_obtainedmarks= obtained_marks1 + obtained_marks2 + obtained_marks3;
+
+// document.write("<h3>Subjects Total Marks Obtained Marks Percentage</h3>")
+
+// document.write(subject1,"&nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp     ",total_marks,"&nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp",obtained_marks1,"&nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp",(obtained_marks1 / total_marks) * 100,"%<br>")
+
+// document.write(subject2,"&nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp     ",total_marks,"&nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp",obtained_marks2,"&nbsp &nbsp &nbsp &nbsp &nbsp  &nbsp",(obtained_marks2 / total_marks) * 100,"%<br>")
+
+//  document.write(subject3,"&nbsp &nbsp &nbsp &nbsp &nbsp",total_marks,"&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp",obtained_marks3,"&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp ",(obtained_marks3 / total_marks) * 100,"%<br>")
+
+// let percentage=( total_obtainedmarks/ 300) * 100;
+
+// document.write("&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp 300 &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp "+ total_obtainedmarks +"&nbsp &nbsp &nbsp &nbsp &nbsp &nbsp"+ percentage);
+
+
+
